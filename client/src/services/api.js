bff271a965ed7a2fcd2788cbef9aefe3b9b9b901
sumitshakya0987 <shakyasumit@gmail.com>
2025-06-21 19:45:11 +0530

    error

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://taskboard-pro-cybc.onrender.com/api',
+  baseURL: 'https://taskboard-pro-cybc.onrender.com',
 })
 
 api.interceptors.request.use((config) => {
